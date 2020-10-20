36d150c8952da18365fa9b8270613f80b3d3e892
mertefe95 <61557140+mertefe95@users.noreply.github.com>
2020-10-20 20:38:43 +0300

    responsiveness of navbar

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -10,28 +10,28 @@ library.add(fab)
 function Nav() {
 
 return(
-<nav className="nav bg-black h-40 flex sm:justify-evenly">       
-    <div className="logo-container md:pl-8">
+<header className="nav bg-black h-40 flex sm:relative">       
+    <div className="logo-container md:ml-8 sm:absolute sm:h-40">
         <Zoom>
-        <a href="/"><img className="h-full py-4 pl-20 sm:pl-12 sm:pr-0" src={RealWhiteLogo}/></a>
+        <a href="/"><img className="h-full py-4 pl-20 sm:pl-0 sm:pr-54" src={RealWhiteLogo}/></a>
         </Zoom> 
     </div>   
 
-    <div className="menu-links-icons font-inter flex text-white items-center pl-64 sm:justify-start sm:pt-12 sm:flex-col sm:pl-0 sm:pr-28 sm:w-5/6 md:flex-col md:pl-32 md:pt-12 xxl:pr-12 ">       
+    <nav className="menu-links-icons font-inter flex text-white items-center pl-64 sm:pt-12 sm:flex-col sm:ml-16 sm:pl-0 sm:pr-0 sm:mr-10 sm:w-5/6 md:flex-col md:pl-32 md:pt-12 xxl:pr-12 ">       
             <Zoom>
-            <ul className="links flex uppercase text-sm font-semibold px-10 sm:pl-0 sm:pr-20">
+            <ul className="links flex uppercase text-sm font-semibold px-10 sm:pl-0 sm:pr-0">
                 <li className="px-4"><a href="/menu">Menu</a></li>
                 <li className="px-4"><a href="/about">About</a></li>
             </ul>
             </Zoom>            
 
-            <ul className="icons flex font-semibold text-xl sm:pl-0 sm:pr-20 sm:pt-4 md:pt-4">
+            <ul className="icons flex font-semibold text-xl sm:pl-0 sm:pr-0 sm:pt-4 md:pt-4">
                 <li className="px-4"><a href="#"><FontAwesomeIcon icon={faInstagram}/></a></li>
                 <li className="px-4"><a href="#"><FontAwesomeIcon icon={faYelp}/></a></li>
                 <li className="px-4"><a href="#"><FontAwesomeIcon icon={faFacebookF}/></a></li>
             </ul>
-    </div>
-</nav>
+    </nav>
+</header>
 
 );
 }
