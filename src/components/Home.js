fdc6763fc4e7fc42bfe0707c94a5c7674ba3da41
mertefe95 <61557140+mertefe95@users.noreply.github.com>
2020-11-04 15:37:10 +0300

    pricing section added

--- src/components/Home.js
+++ src/components/Home.js
@@ -8,102 +8,206 @@ return (
     </div>
 
 
-        
 
-
-
-
-
+<div className="buy-our-product">
+    <div className="bg-gray-100">
+        <div className="pt-12 sm:pt-16 lg:pt-20">
+            <div className="max-w-screen-xl mx-auto px-4 sm:px-6 lg:px-8">
+                <div className="text-center">
+                <h2 className="text-3xl leading-9 font-extrabold text-gray-900 sm:text-4xl sm:leading-10 lg:text-5xl lg:leading-none">
+                Buy Our Product with No Extra Efforts!
+                </h2>
+                <p className="mt-4 text-xl leading-7 text-gray-600">
+                    If you're not satisfied, contact us within the first 14 days and we'll send you a full refund.
+                </p>
+                </div>
+            </div>
+        </div>
     
+        <div className="mt-8 bg-white pb-16 sm:mt-12 sm:pb-20 lg:pb-28">
+            <div className="relative">
+                <div className="absolute inset-0 h-1/2 bg-gray-100"></div>
+                    <div className="relative max-w-screen-xl mx-auto px-4 sm:px-6 lg:px-8">
+                        <div className="max-w-lg mx-auto rounded-lg shadow-lg overflow-hidden lg:max-w-none lg:flex">
+                            <div className="flex-1 bg-white px-6 py-8 lg:p-12">
+                                <h3 className="text-2xl leading-8 font-extrabold text-gray-900 sm:text-3xl sm:leading-9">
+                                New Model of SmartWatch v2
+                                </h3>
+                                <p className="mt-6 text-base leading-6 text-gray-500">
+                                Lorem ipsum dolor sit amet consect etur adipisicing elit. Itaque amet indis perferendis blanditiis repellendus etur quidem assumenda.
+                                </p>
+                            <div className="mt-8">
+                            <div className="flex items-center">
+                                <h4 className="flex-shrink-0 pr-4 bg-white text-sm leading-5 tracking-wider font-semibold uppercase text-indigo-600">
+                                FULL UPDATED NEW VERSION OF SMARTWATCH
+                                </h4>
+                            <div className="flex-1 border-t-2 border-gray-200"></div>
+                            </div>
+                            <ul className="mt-8 lg:grid lg:grid-cols-2 lg:gap-x-8 lg:gap-y-5">
+                                <li className="flex items-start lg:col-span-1">
+                            <div className="flex-shrink-0">
 
+                            <svg className="h-5 w-5 text-green-400" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor">
+                            <path fill-rule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.707-9.293a1 1 0 00-1.414-1.414L9 10.586 7.707 9.293a1 1 0 00-1.414 1.414l2 2a1 1 0 001.414 0l4-4z" clip-rule="evenodd" />
+                            </svg>
+                            </div>
+                                <p className="ml-3 text-sm leading-5 text-gray-700">
+                                Note App with Voice Activation
+                                </p>
+                            </li>
+                            <li className="mt-5 flex items-start lg:col-span-1 lg:mt-0">
+                            <div className="flex-shrink-0">
+                            <svg className="h-5 w-5 text-green-400" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor">
+                            <path fill-rule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.707-9.293a1 1 0 00-1.414-1.414L9 10.586 7.707 9.293a1 1 0 00-1.414 1.414l2 2a1 1 0 001.414 0l4-4z" clip-rule="evenodd" />
+                            </svg>
+                            </div>
+                            <p className="ml-3 text-sm leading-5 text-gray-700">
+                            Fast Photo Taking from Watch Screen
+                            </p>
+                            </li>
+                            <li className="mt-5 flex items-start lg:col-span-1 lg:mt-0">
+                            <div className="flex-shrink-0">
 
-
-
-
-
-
-
+                            <svg className="h-5 w-5 text-green-400" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor">
+                            <path fill-rule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.707-9.293a1 1 0 00-1.414-1.414L9 10.586 7.707 9.293a1 1 0 00-1.414 1.414l2 2a1 1 0 001.414 0l4-4z" clip-rule="evenodd" />
+                            </svg>
+                            </div>
+                            <p className="ml-3 text-sm leading-5 text-gray-700">
+                            Selfie App which takes easy selfies from SmartWatch
+                            </p>
+                            </li>
+                            <li className="mt-5 flex items-start lg:col-span-1 lg:mt-0">
+                                <div div className="flex-shrink-0">
+                                <svg className="h-5 w-5 text-green-400" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor">
+                                <path fill-rule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.707-9.293a1 1 0 00-1.414-1.414L9 10.586 7.707 9.293a1 1 0 00-1.414 1.414l2 2a1 1 0 001.414 0l4-4z" clip-rule="evenodd" />
+                                </svg>
+                                </div>
+                            <p className="ml-3 text-sm leading-5 text-gray-700">
+                            Live Speaking Weather App with Voice 
+                            </p>
+                            </li>
+                        </ul>
+            </div>
+          </div>
+          <div className="py-8 px-6 text-center bg-gray-50 lg:flex-shrink-0 lg:flex lg:flex-col lg:justify-center lg:p-12">
+            <p className="text-lg leading-6 font-medium text-gray-900">
+              Pay once, own it forever
+            </p>
+            <div className="mt-4 flex items-center justify-center text-5xl leading-none font-extrabold text-gray-900">
+              <span>
+                $349
+              </span>
+              <span className="ml-3 text-xl leading-7 font-medium text-gray-500">
+                USD
+              </span>
+            </div>
+            <p className="mt-4 text-sm leading-5">
+              <a href="#" className="font-medium text-gray-500 underline">
+                Learn about our membership policy
+              </a>
+            </p>
+            <div className="mt-6">
+              <div class="rounded-md shadow">
+                <a href="#" className="flex items-center justify-center px-5 py-3 border border-transparent text-base leading-6 font-medium rounded-md text-white bg-gray-900 hover:bg-gray-800 focus:outline-none focus:shadow-outline transition duration-150 ease-in-out">
+                  Get Access
+                </a>
+              </div>
+            </div>
+            <div className="mt-4 text-sm leading-5">
+              <a href="#" className="font-medium text-gray-900">
+                
+                <span className="font-normal text-gray-500">
+                  
+                </span>
+              </a>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</div>
+</div>
 
 
 
 
 <div className="meet-our-team">
+    <div className="bg-white">
+        <div className="mx-auto py-12 px-4 max-w-screen-xl sm:px-6 lg:px-8 lg:py-24">
+            <div className="grid grid-cols-1 gap-12 lg:grid-cols-3 lg:gap-8">
+                <div className="space-y-5 sm:space-y-4">
+                    <h2 className="text-3xl leading-9 font-extrabold tracking-tight sm:text-4xl">Meet our leadership</h2>
+                    <p className="text-xl leading-7 text-gray-500">We try to offer the best quality with our products. We aim to build best SmartWatches of our century.</p>
+                </div>
+                <div className="lg:col-span-2">
+                    <ul className="space-y-12 sm:grid sm:grid-cols-2 sm:gap-12 sm:space-y-0 lg:gap-x-8">
+                        <li>
+                        <div className="flex items-center space-x-4 lg:space-x-6">
+                            <img className="w-16 h-16 rounded-full lg:w-20 lg:h-20" src="https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80" alt="" />
+                            <div className="font-medium text-lg leading-6 space-y-1">
+                            <h4>Leslie Alexander</h4>
+                            <p className="text-indigo-600">Co-Founder / CEO</p>
+                            </div>
+                        </div>
+                        </li>
 
-<div className="bg-white">
-  <div className="mx-auto py-12 px-4 max-w-screen-xl sm:px-6 lg:px-8 lg:py-24">
-    <div className="grid grid-cols-1 gap-12 lg:grid-cols-3 lg:gap-8">
-      <div className="space-y-5 sm:space-y-4">
-        <h2 className="text-3xl leading-9 font-extrabold tracking-tight sm:text-4xl">Meet our leadership</h2>
-        <p className="text-xl leading-7 text-gray-500">We try to offer the best quality with our products. We aim to build best SmartWatches of our century.</p>
-      </div>
-      <div className="lg:col-span-2">
-        <ul className="space-y-12 sm:grid sm:grid-cols-2 sm:gap-12 sm:space-y-0 lg:gap-x-8">
-          <li>
-            <div className="flex items-center space-x-4 lg:space-x-6">
-              <img className="w-16 h-16 rounded-full lg:w-20 lg:h-20" src="https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80" alt="" />
-              <div className="font-medium text-lg leading-6 space-y-1">
-                <h4>Leslie Alexander</h4>
-                <p className="text-indigo-600">Co-Founder / CEO</p>
-              </div>
-            </div>
-          </li>
+                        <li>
+                        <div className="flex items-center space-x-4 lg:space-x-6">
+                            <img className="w-16 h-16 rounded-full lg:w-20 lg:h-20" src="https://images.unsplash.com/photo-1519244703995-f4e0f30006d5?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80" alt="" />
+                            <div className="font-medium text-lg leading-6 space-y-1">
+                            <h4>Michael Foster</h4>
+                            <p className="text-indigo-600">Co-Founder / CTO</p>
+                            </div>
+                        </div>
+                        </li>
 
-          <li>
-            <div className="flex items-center space-x-4 lg:space-x-6">
-              <img className="w-16 h-16 rounded-full lg:w-20 lg:h-20" src="https://images.unsplash.com/photo-1519244703995-f4e0f30006d5?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80" alt="" />
-              <div className="font-medium text-lg leading-6 space-y-1">
-                <h4>Michael Foster</h4>
-                <p className="text-indigo-600">Co-Founder / CTO</p>
-              </div>
-            </div>
-          </li>
+                        <li>
+                        <div className="flex items-center space-x-4 lg:space-x-6">
+                            <img className="w-16 h-16 rounded-full lg:w-20 lg:h-20" src="https://images.unsplash.com/photo-1506794778202-cad84cf45f1d?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80" alt="" />
+                            <div className="font-medium text-lg leading-6 space-y-1">
+                                <h4>Dries Vincent</h4>
+                                <p className="text-indigo-600">Manager, Business Relations</p>
+                            </div>
+                        </div>
+                        </li>
 
-          <li>
-            <div className="flex items-center space-x-4 lg:space-x-6">
-              <img className="w-16 h-16 rounded-full lg:w-20 lg:h-20" src="https://images.unsplash.com/photo-1506794778202-cad84cf45f1d?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80" alt="" />
-              <div className="font-medium text-lg leading-6 space-y-1">
-                <h4>Dries Vincent</h4>
-                <p className="text-indigo-600">Manager, Business Relations</p>
-              </div>
-            </div>
-          </li>
+                        <li>
+                        <div className="flex items-center space-x-4 lg:space-x-6">
+                            <img className="w-16 h-16 rounded-full lg:w-20 lg:h-20" src="https://images.unsplash.com/photo-1517841905240-472988babdf9?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80" alt="" />
+                            <div className="font-medium text-lg leading-6 space-y-1">
+                                <h4>Lindsay Walton</h4>
+                                <p className="text-indigo-600">Product Developer</p>
+                            </div>
+                        </div>
+                        </li>
 
-          <li>
-            <div className="flex items-center space-x-4 lg:space-x-6">
-              <img className="w-16 h-16 rounded-full lg:w-20 lg:h-20" src="https://images.unsplash.com/photo-1517841905240-472988babdf9?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80" alt="" />
-              <div className="font-medium text-lg leading-6 space-y-1">
-                <h4>Lindsay Walton</h4>
-                <p className="text-indigo-600">Product Developer</p>
-              </div>
-            </div>
-          </li>
+                        <li>
+                        <div className="flex items-center space-x-4 lg:space-x-6">
+                            <img className="w-16 h-16 rounded-full lg:w-20 lg:h-20" src="https://images.unsplash.com/photo-1438761681033-6461ffad8d80?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80" alt="" />
+                            <div className="font-medium text-lg leading-6 space-y-1">
+                            <h4>Courtney Henry</h4>
+                            <p className="text-indigo-600">Product Designer</p>
+                            </div>
+                        </div>
+                        </li>
 
-          <li>
-            <div className="flex items-center space-x-4 lg:space-x-6">
-              <img className="w-16 h-16 rounded-full lg:w-20 lg:h-20" src="https://images.unsplash.com/photo-1438761681033-6461ffad8d80?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80" alt="" />
-              <div className="font-medium text-lg leading-6 space-y-1">
-                <h4>Courtney Henry</h4>
-                <p className="text-indigo-600">Product Designer</p>
-              </div>
+                        <li>
+                        <div className="flex items-center space-x-4 lg:space-x-6">
+                            <img className="w-16 h-16 rounded-full lg:w-20 lg:h-20" src="https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80" alt="" /> 
+                            <div className="font-medium text-lg leading-6 space-y-1">
+                            <h4>Tom Cook</h4>
+                            <p className="text-indigo-600">Director, Product Development</p>
+                            </div>
+                        </div>
+                        </li>
+                    </ul>
+                </div>
             </div>
-          </li>
-
-          <li>
-            <div className="flex items-center space-x-4 lg:space-x-6">
-              <img className="w-16 h-16 rounded-full lg:w-20 lg:h-20" src="https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80" alt="" /> 
-              <div className="font-medium text-lg leading-6 space-y-1">
-                <h4>Tom Cook</h4>
-                <p className="text-indigo-600">Director, Product Development</p>
-              </div>
-            </div>
-          </li>
-        </ul>
-      </div>
+        </div>
     </div>
-  </div>
 </div>
 
-</div>
 
             
     
