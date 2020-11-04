d3bc2670b55336680e93b5f8115e26fd34f70ac5
mertefe95 <61557140+mertefe95@users.noreply.github.com>
2020-11-04 15:23:53 +0300

    meet-our-team section has added

--- src/components/Home.js
+++ src/components/Home.js
@@ -3,18 +3,111 @@ import Tailwind from "../assets/tailwind.css";
 
 function Home() {
 return (
-    <main className="homepage-main h-screen">
-        <div className="homepage-background-image h-full">
-
-        </div>
+<main className="homepage-main h-screen">
+    <div className="homepage-background-image h-full">
+    </div>
 
 
         
 
 
+
+
+
+    
+
+
+
+
+
+
+
+
+
+
+
+
+<div className="meet-our-team">
+
+<div className="bg-white">
+  <div className="mx-auto py-12 px-4 max-w-screen-xl sm:px-6 lg:px-8 lg:py-24">
+    <div className="grid grid-cols-1 gap-12 lg:grid-cols-3 lg:gap-8">
+      <div className="space-y-5 sm:space-y-4">
+        <h2 className="text-3xl leading-9 font-extrabold tracking-tight sm:text-4xl">Meet our leadership</h2>
+        <p className="text-xl leading-7 text-gray-500">We try to offer the best quality with our products. We aim to build best SmartWatches of our century.</p>
+      </div>
+      <div className="lg:col-span-2">
+        <ul className="space-y-12 sm:grid sm:grid-cols-2 sm:gap-12 sm:space-y-0 lg:gap-x-8">
+          <li>
+            <div className="flex items-center space-x-4 lg:space-x-6">
+              <img className="w-16 h-16 rounded-full lg:w-20 lg:h-20" src="https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80" alt="" />
+              <div className="font-medium text-lg leading-6 space-y-1">
+                <h4>Leslie Alexander</h4>
+                <p className="text-indigo-600">Co-Founder / CEO</p>
+              </div>
+            </div>
+          </li>
+
+          <li>
+            <div className="flex items-center space-x-4 lg:space-x-6">
+              <img className="w-16 h-16 rounded-full lg:w-20 lg:h-20" src="https://images.unsplash.com/photo-1519244703995-f4e0f30006d5?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80" alt="" />
+              <div className="font-medium text-lg leading-6 space-y-1">
+                <h4>Michael Foster</h4>
+                <p className="text-indigo-600">Co-Founder / CTO</p>
+              </div>
+            </div>
+          </li>
+
+          <li>
+            <div className="flex items-center space-x-4 lg:space-x-6">
+              <img className="w-16 h-16 rounded-full lg:w-20 lg:h-20" src="https://images.unsplash.com/photo-1506794778202-cad84cf45f1d?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80" alt="" />
+              <div className="font-medium text-lg leading-6 space-y-1">
+                <h4>Dries Vincent</h4>
+                <p className="text-indigo-600">Manager, Business Relations</p>
+              </div>
+            </div>
+          </li>
+
+          <li>
+            <div className="flex items-center space-x-4 lg:space-x-6">
+              <img className="w-16 h-16 rounded-full lg:w-20 lg:h-20" src="https://images.unsplash.com/photo-1517841905240-472988babdf9?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80" alt="" />
+              <div className="font-medium text-lg leading-6 space-y-1">
+                <h4>Lindsay Walton</h4>
+                <p className="text-indigo-600">Product Developer</p>
+              </div>
+            </div>
+          </li>
+
+          <li>
+            <div className="flex items-center space-x-4 lg:space-x-6">
+              <img className="w-16 h-16 rounded-full lg:w-20 lg:h-20" src="https://images.unsplash.com/photo-1438761681033-6461ffad8d80?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80" alt="" />
+              <div className="font-medium text-lg leading-6 space-y-1">
+                <h4>Courtney Henry</h4>
+                <p className="text-indigo-600">Product Designer</p>
+              </div>
+            </div>
+          </li>
+
+          <li>
+            <div className="flex items-center space-x-4 lg:space-x-6">
+              <img className="w-16 h-16 rounded-full lg:w-20 lg:h-20" src="https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80" alt="" /> 
+              <div className="font-medium text-lg leading-6 space-y-1">
+                <h4>Tom Cook</h4>
+                <p className="text-indigo-600">Director, Product Development</p>
+              </div>
+            </div>
+          </li>
+        </ul>
+      </div>
+    </div>
+  </div>
+</div>
+
+</div>
+
             
     
-    </main>
+</main>
 );
 
 
